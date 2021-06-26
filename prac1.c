214a0941cb1c2a06b31692fd2a18fa34fa7e3706
2015pushkar <61586999+2015pushkar@users.noreply.github.com>
2021-06-26 14:15:26 +0530

    Uploading 2 files to github

---
+++ prac1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+    printf("Hello World\n");
+    scanf("%d",&n);
+    printf("The output is %d",n);
+    n += 100;
+    printf("updated output is: %d",n);
+
+    return 0;
+}
